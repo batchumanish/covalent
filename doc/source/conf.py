df3f81cce9fb86779203a5ac50ad7c138a684e10
batchumanish <manish.batchu@psiog.com>
2023-04-24 15:54:42 +0530

    concepts/concepts

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -108,5 +108,5 @@ redirects = {
     "troubleshooting":"https://develop.d19pvbre9ckryz.amplifyapp.com/docs/user-documentation/troubleshooting",
     "tutorials/tutorials":"https://develop.d19pvbre9ckryz.amplifyapp.com/docs/user-documentation/tutorials",
     "credentials":"https://develop.d19pvbre9ckryz.amplifyapp.com/docs/user-documentation/credentials/",
-    "concepts":"https://develop.d19pvbre9ckryz.amplifyapp.com/docs/user-documentation/credentials/"
+    "concepts/concepts":"https://develop.d19pvbre9ckryz.amplifyapp.com/docs/user-documentation/credentials/"
 }
