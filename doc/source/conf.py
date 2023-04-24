c5862a3169d20294fd3734449a3c37cf6723fd4e
batchumanish <manish.batchu@psiog.com>
2023-04-24 17:50:16 +0530

    local exec

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -112,5 +112,6 @@ redirects = {
     "concepts/basics":"https://develop.d19pvbre9ckryz.amplifyapp.com/docs/user-documentation/credentials/",
     "concepts/architecture":"https://develop.d19pvbre9ckryz.amplifyapp.com/docs/user-documentation/credentials/",
     "concepts/api_concepts":"https://develop.d19pvbre9ckryz.amplifyapp.com/docs/user-documentation/credentials/",
-    "webapp_ui/dashboard/index":"https://develop.d19pvbre9ckryz.amplifyapp.com/docs/user-documentation/credentials/"
+    "webapp_ui/dashboard/index":"https://develop.d19pvbre9ckryz.amplifyapp.com/docs/user-documentation/credentials/",
+    "api/executors/local":"https://develop.d19pvbre9ckryz.amplifyapp.com/docs/user-documentation/credentials/"
 }
